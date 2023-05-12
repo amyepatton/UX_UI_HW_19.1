79d583c14aecd6c6f099d1697595c26d2078c7ef
amyepatton <116026940+amyepatton@users.noreply.github.com>
2023-05-12 17:16:01 -0400

    updated scroll to top js

--- js/index.js
+++ js/index.js
@@ -18,3 +18,7 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+$("#myBtn").on("click", function() {
+    console.log("You clicked on #myBtn");
+});
